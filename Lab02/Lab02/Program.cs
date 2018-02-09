68a5f208c91dd5f49c8e454828807f5fb23b73f0
HeidiHouzenga <35928905+HeidiHouzenga@users.noreply.github.com>
2018-02-09 09:14:17 -0500

    lab02

--- Lab02/Lab02/Program.cs
+++ Lab02/Lab02/Program.cs
@@ -1,41 +1,46 @@
 ﻿using System;
 
-   class Addition
+namespace Lab02
 {
-       static void Main()
-       {
-        int number2;
-        int sum;
 
-        Console.Write("enter first integer: ");
-        //addition-question2
-        int number1 = int.Parse(Console.ReadLine());
-        Console.Write("Enter second integer:  ");
-        number2 = int.Parse(Console.ReadLine());
+    class Addition
+    {
+        static void Main()
+        {
+            //addition-question2
+            int number1;
+            int number2;
+            int sum;
 
-        sum = number1 + number2;
+            Console.Write("enter first integer: ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter second integer:  ");
+            number2 = int.Parse(Console.ReadLine());
 
-        Console.WriteLine($"Sum is {sum}");
+            sum = number1 + number2;
 
-        //question 3
-        Console.WriteLine("Solution to the expression (x+y)*(z+10) as x=1, y=2, and z=5");
-        int x = 1;
-        int y = 2;
-        int z = 5;
-        int solution;
-        solution = ((x + y) * (z + 10));
+            Console.WriteLine($"Sum is {sum}");
 
-        Console.WriteLine((x + y) * (z + 10));
+            //question 3
+            Console.WriteLine("Solution to the expression (x+y)*(z+10) as x=1, y=2, and z=5");
+            int x = 1;
+            int y = 2;
+            int z = 5;
+            int solution;
+            solution = ((x + y) * (z + 10));
+
+            Console.WriteLine((x + y) * (z + 10));
 
 
 
 
-        //question 4
-        Console.WriteLine("Hello\tWorld");
-        Console.WriteLine("Hello\nWorld");
-        Console.WriteLine("\"Hello World\"");
-        Console.WriteLine("Hello\\World");
+            //question 4
+            Console.WriteLine("Hello\tWorld");
+            Console.WriteLine("Hello\nWorld");
+            Console.WriteLine("\"Hello World\"");
+            Console.WriteLine("Hello\\World");
         }
+    }
 }
 
 
