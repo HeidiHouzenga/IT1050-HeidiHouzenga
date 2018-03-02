291ab4910f6078353d74f177b9b24d9ad781348e
HeidiHouzenga <35928905+HeidiHouzenga@users.noreply.github.com>
2018-03-02 18:53:06 -0500

    Add files via upload

---
+++ Lab 6-1/Lab 6-1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab_6_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int counter = 1;
+            int year = 2018;
+           
+            while (counter <= 20)
+            {
+
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+                {
+                    ++counter;
+                    Console.WriteLine(year);
+                }
+
+                ++year;
+             
+             }
+          
+          
+        }
+    }
+}
