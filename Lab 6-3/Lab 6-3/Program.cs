e4ba517da1c9fae6160b43ce2fd8018f5185cde8
HeidiHouzenga <35928905+HeidiHouzenga@users.noreply.github.com>
2018-03-03 10:57:58 -0500

    Add files via upload

---
+++ Lab 6-3/Lab 6-3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Lab_6_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            string item;
+            string allItems = "";
+            decimal cost;
+            decimal totalCost = 0;
+            string input;
+                      
+
+            Console.Write("Please enter the next item: ");
+            item =Console.ReadLine();
+
+            Console.Write("Please enter the item cost: ");
+            cost = decimal.Parse(Console.ReadLine());
+
+
+            Console.Write("Press <enter> to continue, E to end: ");
+            input = Console.ReadLine();
+
+        while (input != "E")
+        {
+                allItems += item + "\n";
+                totalCost += cost;
+                Console.Write("Please enter the next item: ");
+                item = Console.ReadLine();
+
+                Console.Write("Please enter the item cost: ");
+                cost = decimal.Parse(Console.ReadLine());
+
+
+                Console.Write("Press <enter> to continue, E to end: ");
+                input = Console.ReadLine();
+
+            }
+
+            allItems += item + "\n";
+            totalCost += cost;
+
+            Console.WriteLine("\n"  + "Grocery Items: ");
+            Console.WriteLine(allItems + "\n");
+
+            Console.WriteLine("Total Costs: " + totalCost);
+
+        }
+     }
+}
